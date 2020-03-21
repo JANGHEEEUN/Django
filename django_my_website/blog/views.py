71808e8b7ce4b98075174ebb84e7df40ea2ac6ae
JANGHEEEUN <qhqhclsrn@naver.com>
2020-03-21 18:58:11 +0900

    update

--- django_my_website/blog/views.py
+++ django_my_website/blog/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
 
-def index(request):
-    posts = Post.objects.all()
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts,
-            'a_plus_b': 1+3,
-        }
-    )
+
+
+# def index(request):
+#     posts = Post.objects.all()
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts,
+#             'a_plus_b': 1+3,
+#         }
+#     )
