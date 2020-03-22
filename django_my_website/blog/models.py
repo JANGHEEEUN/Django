eda066865ec1766b17ec48734a764668203ff855
JANGHEEEUN <qhqhclsrn@naver.com>
2020-03-22 14:24:36 +0900

    categories in post_detail page

--- django_my_website/blog/models.py
+++ django_my_website/blog/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Category(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -10,6 +19,8 @@ class Post(models.Model):
 
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=True)
+
+    category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
