8af135668961094f41e7560181926cfadc1db978
JANGHEEEUN <qhqhclsrn@naver.com>
2020-03-22 16:14:14 +0900

    post list by category

--- django_my_website/blog/views.py
+++ django_my_website/blog/views.py
@@ -19,10 +19,39 @@ class PostDetail(DetailView):
     model = Post
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(PostList, self).get_context_data(**kwargs)
+        context = super(PostDetail, self).get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
         context['posts_without_category'] = Post.objects.filter(category=None).count()
 
+        return context
+
+class PostListByCategory(ListView):
+
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+
+        if slug == '_none':
+            category = None
+        else:
+            category = Category.objects.get(slug=slug)
+
+        return Post.objects.filter(category=category).order_by('-created')
+
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(type(self), self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
+
+        slug = self.kwargs['slug']
+
+        if slug == '_none':
+            context['category'] = '미분류'
+        else:
+            category = Category.objects.get(slug=slug)
+            context['category'] = category
+
+        # context['title'] = 'Blog - {}'.format(category.name)
         return context
 
 # def post_detail(request, pk):
@@ -46,4 +75,5 @@ class PostDetail(DetailView):
 #             'posts':posts,
 #             'a_plus_b': 1+3,
 #         }
-#     )
+#
+
