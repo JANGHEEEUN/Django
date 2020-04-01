75db2235e9bacf2e88f694ee96bf72374ac52ba7
JANGHEEEUN <qhqhclsrn@naver.com>
2020-04-01 16:52:44 +0900

    project_detail-separate 2 part

--- web/main/models.py
+++ web/main/models.py
@@ -5,9 +5,11 @@ class Project(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=50)
     created = models.DateTimeField()
+    image = models.ImageField(upload_to='img/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return '{}'.format(self.title)
 
     def get_absolute_url(self):
         return '/project/{}/'.format(self.pk)
+    
